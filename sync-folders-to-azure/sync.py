0af572614e371948a62fd2da312c417e41c8017e
Bad Numbers <badnumbersmusic@gmail.com>
2025-03-01 21:48:07 +0000

    Added printing the python version.

--- sync-folders-to-azure/sync.py
+++ sync-folders-to-azure/sync.py
@@ -1,10 +1,14 @@
 import json
 import os
+import sys
+
+def do_nothing():
+   cats = "fish"
 
 def validate_config(config):
   if ("PathToFolder" in config):
       if os.path.isdir(config["PathToFolder"]):
-          print("All is well")
+         do_nothing()
       else:
          raise Exception("The config file's 'PathToFolder' property is not a folder in the file system.")    
   else:
@@ -14,3 +18,5 @@ with open('config.json', 'r') as file:
     config = json.load(file)
 
 validate_config(config)
+
+print(f"Running script using Python version {sys.version}.")
